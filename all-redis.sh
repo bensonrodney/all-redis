c75bab7ba2e25e5bc07766d06a875e1939819ceb
Jason Milen <jason.milen@opengear.com>
2019-09-04 15:31:03 +1000

    allow the redis server port to be specified and handle the reading of sets while maintaing the grepable key at the start of each line. Also show the type if the key is a set or list

--- all-redis.sh
+++ all-redis.sh
@@ -1,15 +1,57 @@
 #!/bin/bash
 
+port=6379
+
+function usage() {
+	cat <<EOF
+Usage:
+	$(basename ${0}) [-p PORT]
+
+EOF
+}
+
 function print_list() {
 	key=$1
-	listdata=$(redis-cli LRANGE ${key} 0 -1 | sed -r "s/^/${key} /g")
+	listdata=$(redis-cli -p ${port} LRANGE ${key} 0 -1 | sed -r "s/^/${key} (LIST) /g")
 	echo -e "${listdata}"
 }
 
-for key in $(redis-cli keys \* | sort) ; do
-	if [[ $(redis-cli TYPE ${key}) = 'list' ]] ; then
-		print_list ${key}
-	else
-		echo "$key - $(redis-cli GET $key)"
-	fi
+function print_set() {
+	key=$1
+	setdata=$(redis-cli -p ${port} SMEMBERS ${key} | sed -r "s/^/${key} (SET) /g")
+	echo -e "${setdata}"
+}
+
+while getopts "p:h" o; do
+    case "${o}" in
+        p)
+            port=${OPTARG}
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: ${o}"
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+for key in $(redis-cli -p ${port} keys \* | sort) ; do
+
+	case "$(redis-cli -p ${port} TYPE ${key})" in
+		list)
+			print_list ${key}
+			;;
+		set)
+			print_set ${key}
+			;;
+		*)
+			echo "$key - $(redis-cli -p ${port} GET $key)"
+			;;
+	esac
 done
