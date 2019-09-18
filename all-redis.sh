d77badc2f70a58dce59c0d1f0ff754b870d3d94c
Jason Milen <jason.milen@opengear.com>
2019-09-18 12:41:13 +1000

    add support for specifying the redis host

--- all-redis.sh
+++ all-redis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rcmd="redis-cli"
 port=6379
+server="localhost"
 
 function usage() {
 	cat <<EOF
@@ -12,20 +14,23 @@ EOF
 
 function print_list() {
 	key=$1
-	listdata=$(redis-cli -p ${port} LRANGE ${key} 0 -1 | sed -r "s/^/${key} (LIST) /g")
+	listdata=$(${rcmd} LRANGE ${key} 0 -1 | sed -r "s/^/${key} (LIST) /g")
 	echo -e "${listdata}"
 }
 
 function print_set() {
 	key=$1
-	setdata=$(redis-cli -p ${port} SMEMBERS ${key} | sed -r "s/^/${key} (SET) /g")
+	setdata=$(${rcmd} SMEMBERS ${key} | sed -r "s/^/${key} (SET) /g")
 	echo -e "${setdata}"
 }
 
-while getopts "p:h" o; do
+while getopts "p:s:h" o; do
     case "${o}" in
         p)
             port=${OPTARG}
+            ;;
+        s)
+            server=${OPTARG}
             ;;
         h)
             usage
@@ -40,10 +45,11 @@ while getopts "p:h" o; do
 done
 shift $((OPTIND-1))
 
+rcmd="redis-cli -p ${port} -h ${server}"
 
-for key in $(redis-cli -p ${port} keys \* | sort) ; do
+for key in $(${rcmd} keys \* | sort) ; do
 
-	case "$(redis-cli -p ${port} TYPE ${key})" in
+	case "$(${rcmd} TYPE ${key})" in
 		list)
 			print_list ${key}
 			;;
@@ -51,7 +57,7 @@ for key in $(redis-cli -p ${port} keys \* | sort) ; do
 			print_set ${key}
 			;;
 		*)
-			echo "$key - $(redis-cli -p ${port} GET $key)"
+			echo "$key - $(${rcmd} GET $key)"
 			;;
 	esac
 done
